c00873091d63ceb96d62218d4797ca26dd451cb3
Thiago Costa <thiagohrcosta86@gmail.com>
2021-04-26 21:35:41 -0300

    Model Propertie created

---
+++ app/models/propertie.rb
@@ -0,0 +1,3 @@
+class Propertie < ApplicationRecord
+  belongs_to :type
+end
---
+++ test/models/propertie_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class PropertieTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
